55b730c8606f7d19e709d70836f7dd3cc7a956e3
kkodachii <153582302+kkodachii@users.noreply.github.com>
2025-04-27 11:22:03 +0800

    fixed minor mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { UploadLanguageComponent } from './components/upload-language/upload-language.component';
 import { TranscriptComponent } from './components/transcript/transcript.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UploadLanguageComponent,TranscriptComponent ],
+  imports: [CommonModule, HeaderComponent, UploadLanguageComponent, TranscriptComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+    const sidebar = document.querySelector('aside');
+    if (sidebar) {
+      sidebar.classList.toggle('-translate-x-full');
+    }
+  }
+}
